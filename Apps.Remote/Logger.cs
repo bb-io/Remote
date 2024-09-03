a8993c73e0e70128b3fc35ce123b7e8342f1ddda
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-09-03 16:56:27 +0300

    Added logging

--- Apps.Remote/Logger.cs
+++ Apps.Remote/Logger.cs
@@ -5,7 +5,7 @@ namespace Apps.Remote;
 
 public class Logger
 {
-    private static string LogUrl = "https://webhook.site/d05d7a10-f7cd-4917-8ba2-a30ee72b6719";
+    private static string LogUrl = "https://webhook.site/e90dd727-1a6b-40ee-8fbb-4b33ff7d65f2";
 
     public static async Task LogAsync<T>(T obj) where T : class
     {
--- Apps.Remote/Polling/PollingList.cs
+++ Apps.Remote/Polling/PollingList.cs
@@ -72,49 +72,59 @@ public class PollingList(InvocationContext invocationContext) : AppInvocable(inv
     public async Task<PollingEventResponse<PageMemory, InvoicesResponse>> OnInvoiceAdded(
         PollingEventRequest<PageMemory> request)
     {
-        var invoices = await SearchInvoices(new SearchInvoicesRequest());
-        var memories = request.Memory?.PageMemoryDtos ?? new List<PageMemoryDto>();
-        var newInvoices = invoices.Invoices!
-            .Where(x => memories.All(m => m.Id != x.Id))
-            .ToList();
-
-        if (request.Memory is null)
+        try
         {
-            return new PollingEventResponse<PageMemory, InvoicesResponse>
+            var invoices = await SearchInvoices(new SearchInvoicesRequest());
+            var memories = request.Memory?.PageMemoryDtos ?? new List<PageMemoryDto>();
+            var newInvoices = invoices.Invoices!
+                .Where(x => memories.All(m => m.Id != x.Id))
+                .ToList();
+
+            await Logger.LogAsync(new { Memory = request.Memory, NewInvoices = newInvoices });
+
+            if (request.Memory is null)
             {
-                FlyBird = false,
-                Memory = new PageMemory
+                return new PollingEventResponse<PageMemory, InvoicesResponse>
                 {
-                    PageMemoryDtos = newInvoices.Select(x => new PageMemoryDto { Id = x.Id, Status = x.Status })
-                        .ToList()
-                },
-                Result = null
-            };
-        }
+                    FlyBird = false,
+                    Memory = new PageMemory
+                    {
+                        PageMemoryDtos = newInvoices.Select(x => new PageMemoryDto { Id = x.Id, Status = x.Status })
+                            .ToList()
+                    },
+                    Result = null
+                };
+            }
 
-        if (newInvoices.Count == 0)
-        {
+            if (newInvoices.Count == 0)
+            {
+                return new PollingEventResponse<PageMemory, InvoicesResponse>
+                {
+                    FlyBird = false,
+                    Memory = request.Memory,
+                    Result = null
+                };
+            }
+
+            memories.AddRange(newInvoices.Select(x => new PageMemoryDto { Id = x.Id, Status = x.Status }));
             return new PollingEventResponse<PageMemory, InvoicesResponse>
             {
-                FlyBird = false,
-                Memory = request.Memory,
-                Result = null
+                FlyBird = true,
+                Memory = new PageMemory { PageMemoryDtos = memories },
+                Result = new InvoicesResponse
+                {
+                    TotalCount = newInvoices.Count,
+                    CurrentPage = 1,
+                    TotalPages = 1,
+                    Invoices = newInvoices
+                }
             };
         }
-
-        memories.AddRange(newInvoices.Select(x => new PageMemoryDto { Id = x.Id, Status = x.Status }));
-        return new PollingEventResponse<PageMemory, InvoicesResponse>
-        {
-            FlyBird = true,
-            Memory = new PageMemory { PageMemoryDtos = memories },
-            Result = new InvoicesResponse
-            {
-                TotalCount = newInvoices.Count,
-                CurrentPage = 1,
-                TotalPages = 1,
-                Invoices = newInvoices
-            }
-        };
+        catch (Exception e)
+        {            
+            await Logger.LogAsync(e);
+            throw;
+        }
     }
     
     private async Task<InvoicesResponse> SearchInvoices([ActionParameter] SearchInvoicesRequest request)
