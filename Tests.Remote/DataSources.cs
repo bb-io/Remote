e5b102a8a55a7bdf65b4b9cbf56c4de59ded9cb6
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-11-25 16:17:44 +0200

    use file-scoped namespace in test classes

--- Tests.Remote/DataSources.cs
+++ Tests.Remote/DataSources.cs
@@ -2,48 +2,47 @@
 using Blackbird.Applications.Sdk.Common.Dynamic;
 using Remote.Base;
 
-namespace Tests.Remote
+namespace Tests.Remote;
+
+[TestClass]
+public class DataSources : TestBase
 {
-    [TestClass]
-    public class DataSources : TestBase
+    [TestMethod]
+    public async Task EmploymentDataHandler_ReturnsValues()
     {
-        [TestMethod]
-        public async Task EmploymentDataHandler_ReturnsValues()
+        var handler= new EmploymentDataSource(InvocationContext);
+        var result = await handler.GetDataAsync(new DataSourceContext { SearchString = "" }, CancellationToken.None);
+
+        foreach (var item in result)
         {
-            var handler= new EmploymentDataSource(InvocationContext);
-            var result = await handler.GetDataAsync(new DataSourceContext { SearchString = "" }, CancellationToken.None);
-
-            foreach (var item in result)
-            {
-                Console.WriteLine($"Id: {item.Key}, Name: {item.Value}");
-            }
-            Assert.IsNotNull(result);
+            Console.WriteLine($"Id: {item.Key}, Name: {item.Value}");
         }
+        Assert.IsNotNull(result);
+    }
 
-        [TestMethod]
-        public async Task EmploymentShortIdDataHandler_ReturnsValues()
+    [TestMethod]
+    public async Task EmploymentShortIdDataHandler_ReturnsValues()
+    {
+        var handler = new EmploymentShortIdDataSource(InvocationContext);
+        var result = await handler.GetDataAsync(new DataSourceContext { SearchString = "" }, CancellationToken.None);
+
+        foreach (var item in result)
         {
-            var handler = new EmploymentShortIdDataSource(InvocationContext);
-            var result = await handler.GetDataAsync(new DataSourceContext { SearchString = "" }, CancellationToken.None);
-
-            foreach (var item in result)
-            {
-                Console.WriteLine($"Id: {item.Key}, Name: {item.Value}");
-            }
-            Assert.IsNotNull(result);
+            Console.WriteLine($"Id: {item.Key}, Name: {item.Value}");
         }
+        Assert.IsNotNull(result);
+    }
 
-        [TestMethod]
-        public async Task CountryDataSource_ReturnsCountryCodes()
+    [TestMethod]
+    public async Task CountryDataSource_ReturnsCountryCodes()
+    {
+        var handler = new CountryDataSource(InvocationContext);
+        var result = await handler.GetDataAsync(new DataSourceContext { SearchString = "" }, CancellationToken.None);
+
+        foreach (var item in result)
         {
-            var handler = new CountryDataSource(InvocationContext);
-            var result = await handler.GetDataAsync(new DataSourceContext { SearchString = "" }, CancellationToken.None);
-
-            foreach (var item in result)
-            {
-                Console.WriteLine($"Id: {item.Key}, Name: {item.Value}");
-            }
-            Assert.IsNotNull(result);
+            Console.WriteLine($"Id: {item.Key}, Name: {item.Value}");
         }
+        Assert.IsNotNull(result);
     }
 }
